7e917d17714abfc1b744ca1b950ee67fb30231e6
Bhoomika <bhoomikahm18@gmail.com>
2023-10-06 10:44:23 +0530

    Adding env file

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ dotenv.config({ path: './.env'});
 const app = express();
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root123',
-    database: 'nodejs-login'
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
 })
 
 db.connect((err) => {
