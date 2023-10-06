71ee81759253500e9c582b47bfa9eb99b84c80b0
Bhoomika <bhoomikahm18@gmail.com>
2023-10-06 10:35:29 +0530

    Connecting to mysql

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env'});
 const app = express();
 
-app.get("/", (req,res)=>{
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root123',
+    database: 'nodejs-login'
+})
+
+db.connect((err) => {
+    if (err) {
+        console.log();
+    } else {
+        console.log("MySQL Connected");
+    }
+})
+
+app.get("/", (req, res) => {
     res.send("<h1>Home Page</h1>")
 })
 
-app.listen(5000, ()=>{
+app.listen(5000, () => {
     console.log("Server Started on port 5000");
 })
